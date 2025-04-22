9194716129f608f0f8a309bb3e23d5b2691e3431
RavikumarAkuthotaSf4250 <152277499+RavikumarAkuthotaSf4250@users.noreply.github.com>
2025-04-22 17:52:44 +0530

    Update App.xaml.cs

--- ChatMaui/App.xaml.cs
+++ ChatMaui/App.xaml.cs
@@ -7,8 +7,11 @@ namespace ChatMaui
         public App()
         {
             InitializeComponent();
+        }
 
-            MainPage = new MainPage();
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage());
         }
     }
 }
